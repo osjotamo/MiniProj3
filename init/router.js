bd6a9b14dbd7e92d3507a4b9ded62bbfd632ea5f
osjotamo <osjotamo@gmail.com>
2023-02-11 19:19:47 +0200

    Create router.js

---
+++ init/router.js
@@ -0,0 +1,14 @@
+module.exports = (app) => {
+
+    app.use('/', require("../routes/home.routes"));
+    app.use('/animals', require('../routes/animal.routes'));
+    app.use('/auth', require('../routes/auth.routes'));
+    app.use('/questions', require('../routes/question.routes'));
+    app.use('/quizzes', require('../routes/quiz.routes'));
+    app.use('/users/levels', require('../routes/userlevel.routes'));
+    app.use('/users', require('../routes/user.routes'));
+    app.use('/emails', require('../routes/email.routes'));
+    app.use('/patrocinadores', require('../routes/sponsor.routes'));
+    app.use('/especialistas', require('../routes/especialista.routes'));
+
+}
