e9d9548e85072d014778b137e600864cd105924c
osjotamo <osjotamo@gmail.com>
2023-02-11 19:21:31 +0200

    Create sponsor.routes.js

---
+++ routes/sponsor.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+let router = express.Router();
+const SponsorController = require('../controllers/sponsor.controller');
+const {
+    body,
+    param,
+    sanitizeBody
+} = require('express-validator');
+const CONFIG = require("../config/config");
+const AuthController = require("../controllers/auth.controller");
+
+router.route('/')
+    .get(AuthController.checkAuth, SponsorController.get)
+    .post(AuthController.checkAuth, [body('name').isString(),
+        body('group').isString(),
+        body('description').isString(),
+        body('level').isInt(),
+        body('links.*.types').isAlpha(),
+        body('links.*.url').isURL(),
+        sanitizeBody('description').whitelist(CONFIG.sanitize.alphabet + CONFIG.sanitize.numerical)
+    ], SponsorController.create);
+
+router.route('/:id')
+    .get(AuthController.checkAuth, [param("id").isMongoId()], SponsorController.getOne)
+    .put(AuthController.checkAuth, [param("id").isMongoId()], SponsorController.update)
+    .delete(AuthController.checkAuth, [param("id").isMongoId()], SponsorController.delete);
+
+module.exports = router;
